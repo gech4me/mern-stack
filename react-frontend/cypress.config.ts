e8a6e75694ef1d3ecdec8688d13a8989ef2149fc
Getachew Mulat <gech4me@gmail.com>
2022-11-07 07:57:23 +0300

    react-frontend updated

---
+++ react-frontend/cypress.config.ts
@@ -0,0 +1,16 @@
+export default {
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require("./cypress/plugins/index.js")(on, config);
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+    },
+  },
+};
